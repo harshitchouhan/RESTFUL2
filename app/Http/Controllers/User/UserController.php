64a680fbc4720f9bf5bed52b23ad9623fc25e19a
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-04-16 08:58:47 +0530

    Implemented CROS

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -129,6 +129,12 @@ class UserController extends ApiController
         return $this->showOne($user);
     }
 
+    public function me($request)
+    {
+        $user = $request->user();
+        return $this->showOne($user);
+    }
+
     public function verify($token)
     {
         $user = User::where('verification_token', $token)->firstOrFail();
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'cors',
             'signature:X-Application-Name',
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -58,6 +59,7 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'cors' => \Fruitcake\Cors\HandleCors::class,
         'client.credentials' => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
